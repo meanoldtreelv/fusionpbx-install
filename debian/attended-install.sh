e7d0f41b51f7a65166e13923db1107e9758a6645
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-07-13 16:16:14 -0700

    clean up nginx config script and merge into main install script

--- debian/attended-install.sh
+++ debian/attended-install.sh
@@ -146,10 +146,6 @@ chown -R www-data:www-data /var/www/fusionpbx
 #send a message
 verbose "Configuring PHP"
 
-#set php version
-php_version=7.4
-
-
 #install dependencies
 apt-get install -y nginx
 apt-get install -y php7.4 php7.4-cli php7.4-dev php7.4-fpm php7.4-pgsql php7.4-sqlite3 php7.4-odbc php7.4-curl php7.4-imap php7.4-xml php7.4-gd php7.4-mbstring
@@ -187,7 +183,37 @@ systemctl restart php7.4-fpm
 read -p "PHP installation complete, check for errors."
 
 #NGINX web server
-resources/nginx.sh
+#send a message
+verbose "configuring the web server"
+
+
+#enable fusionpbx nginx config
+cp nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
+
+#prepare socket name
+sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php7.4-fpm.sock;#g'
+
+ln -s /etc/nginx/sites-available/fusionpbx /etc/nginx/sites-enabled/fusionpbx
+
+#self signed certificate
+ln -s /etc/ssl/private/ssl-cert-snakeoil.key /etc/ssl/private/nginx.key
+ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/ssl/certs/nginx.crt
+
+#remove the default site
+rm /etc/nginx/sites-enabled/default
+
+#add the letsencrypt directory
+if [ .$letsencrypt_folder = .true ]; then
+        mkdir -p /var/www/letsencrypt/
+fi
+
+#flush systemd cache
+systemctl daemon-reload
+
+#restart nginx
+service nginx restart
+
+read -p "web server configured, check for errors."
 
 #FreeSWITCH
 resources/switch.sh
