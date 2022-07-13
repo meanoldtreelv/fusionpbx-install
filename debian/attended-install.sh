d2a0fa8c8492c60beb22121f009ff4dad25f8328
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-07-13 15:58:01 -0700

    move fusionpbx install to main script

--- debian/attended-install.sh
+++ debian/attended-install.sh
@@ -112,7 +112,35 @@ read -p "sngrep installation complete, check output for any errors."
 clear
 
 #FusionPBX
-resources/fusionpbx.sh
+#send a message
+verbose "Installing FusionPBX"
+
+#install dependencies
+apt-get install -y vim git dbus haveged ssl-cert qrencode
+apt-get install -y ghostscript libtiff5-dev libtiff-tools at
+
+#pause to verify installation
+read -p "FusionPBX dependencies installed, check for errors."
+
+#get the branch
+if [ .$system_branch = .'master' ]; then
+	verbose "Using master"
+	branch=""
+else
+	system_major=$(git ls-remote --heads https://github.com/fusionpbx/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
+	system_minor=$(git ls-remote --tags https://github.com/fusionpbx/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
+	system_version=$system_major.$system_minor
+	verbose "Using version $system_version"
+	branch="-b $system_version"
+fi
+
+#add the cache directory
+mkdir -p /var/cache/fusionpbx
+chown -R www-data:www-data /var/cache/fusionpbx
+
+#get the source code
+git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+chown -R www-data:www-data /var/www/fusionpbx
 
 #PHP
 resources/php.sh
