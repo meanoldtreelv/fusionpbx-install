8a88e3e5f35124d0208d4a7134879c60a3f11d2c
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-07-15 10:16:42 -0700

    update initial package load

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ rm -rf /usr/src/fusionpbx-install
 #grab the base packages
 apt-get update
 apt-get upgrade
-apt-get install -y git lsb-release wget
+apt-get install -y git lsb-release wget nano
 # cloning the repo so that we have things on disk
 cd /usr/src
 git clone https://github.com/meanoldtreelv/fusionpbx-install.git
-./usr/src/fusionpbx-install/installmenu.sh
+. ./usr/src/fusionpbx-install/installmenu.sh
