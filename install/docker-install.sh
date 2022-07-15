ff517110990513bb0c0832482e4904ff7fda2602
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-07-15 10:20:32 -0700

    remove read statement

--- install/docker-install.sh
+++ install/docker-install.sh
@@ -9,9 +9,6 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-read -r -n 1 -s -p "check to see if docker repo was installed correctly"
-
-
 #install docker packages
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
