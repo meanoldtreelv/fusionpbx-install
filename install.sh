6af23feb2617fe1bb97fdb2441a00dfbaf574d3d
Cody Launius <claunius@ztelco.com>
2022-07-14 18:09:42 -0700

    create new install scripts

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# installation bootstrap script 
+#
+#clean up previous install attempts
+rm -rf /usr/src/fusionpbx-install
+#grab the base packages
+apt-get update
+apt-get upgrade
+apt-get install -y git lsb-release wget
+# cloning the repo so that we have things on disk
+cd /usr/src
+git clone https://github.com/meanoldtreelv/fusionpbx-install.git
+./usr/src/fusionpbx-install/installmenu.sh
