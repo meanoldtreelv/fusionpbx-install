b163bf42d06b510e9fd1da059de944fafb2de2a3
meanoldtreelv <97059644+meanoldtreelv@users.noreply.github.com>
2022-07-15 17:43:37 -0700

    swap bash for source

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ apt-get install -y git lsb-release nano
 # cloning the repo so that we have things on disk
 cd /usr/src
 git clone https://github.com/meanoldtreelv/fusionpbx-install.git
-bash /usr/src/fusionpbx-install/installmenu.sh
+source /usr/src/fusionpbx-install/installmenu.sh
